1bfc311d46a19b60c72fb66059c1c5094081aa48
IsmaelGutierrez91 <ismael02guti08@gmail.com>
2025-04-24 12:23:19 -0500

    Player Movement

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -4,5 +4,45 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody _componentRigidbody;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float playerJumpStrength;
+    private float horizontalX;
+    private float horizontalZ;
+    bool canJump = false;
+
+    [SerializeField] Transform originPoint;
+    [SerializeField] float rayCastLength;
+    [SerializeField] LayerMask layerMask;
+    public Color inCollision = Color.white;
+    public Color outCollision = Color.white;
+
+    private void Awake()
+    {
+        _componentRigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        _componentRigidbody.linearVelocity = new Vector3(horizontalX * playerSpeed, _componentRigidbody.linearVelocity.y, horizontalZ * playerSpeed);
+        RaycastHit hit;
+        if (Physics.Raycast(originPoint.position, Vector3.down, out hit, rayCastLength, layerMask))
+        {
+            Debug.DrawRay(originPoint.position, Vector3.down * hit.distance, inCollision);
+            canJump = true;
+        }
+        else
+        {
+            Debug.DrawRay(originPoint.position, Vector3.down * rayCastLength, outCollision);
+            canJump = false;
+        }
+    }
+    public void OnMovementX(InputAction.CallbackContext context)
+    {
+        horizontalX = context.ReadValue<float>();
+    }
+    public void OnMovementZ(InputAction.CallbackContext context)
+    {
+        horizontalZ = context.ReadValue<float>();
+    }
 
 }
